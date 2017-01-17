9c2c06098cda2e4afc73570997d5471a10f9fc72
samyang <1129993231@qq.com>
2017-01-17 12:02:26 +0800

    Initial commit

---
+++ Test1/src/cn/tarena/Test01.java
@@ -0,0 +1,43 @@
+package cn.tarena;
+
+/**
+ * Created by Administrator on 2017/1/16 0016.
+ *
+ */
+public class Test01 {
+    //psvm快捷提示main方法
+    public static void main(String[] args) {
+        //System.out.println()快捷键为sout
+        System.out.println("haha");
+        //for循环提示fori
+        for (int i = 0; i <4 ; i++) {
+
+        }
+    }
+
+
+
+
+
+
+    public void test(){
+        int a=10;
+        for (int i=0;i<a;i++){
+            System.out.println("hello");
+        }
+    }
+
+}
+
+class Dog{
+        private String name="aa";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
